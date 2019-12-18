4113e76e797c54cf513313f95c1c6ac84c322830
medhasm <57920502+medhasm@users.noreply.github.com>
2019-12-18 14:10:47 +0200

    Update Insert.c

--- Insert.c
+++ Insert.c
@@ -1,29 +1,46 @@
 #include <stdio.h>
 #include "Insert.h"
+
 void shift_element(int* arr, int i){
 	if(i>0)
 	{		
 int *p;
 p=arr;
-for(i>0;i--)
+for(int z=i;z>0;z--)
 {
-*(p+i)=*(p+i-1);
+*(p+z)=*(p+z-1);
 }
-*p=NULL;
+*p=0;
 	}
 }
 void insertion_sort(int* arr , int len){
 	for(int i=1;i<len;i++){
 		int j=i-1;
-		int counter=;
-		while(*(arr+i)<*(arr+j) && j>=){
-			j--;
+		int counter=0;
+if(*(arr+i)>*(arr+j)){
+ continue;
+}
+		while(*(arr+i)<*(arr+j) && j>=0){
+			if(j==0){
+			j=0;
 			counter+=1;
+				break;
+			}else{
+			j-=1;
+
+			counter+=1;
+}
+
 		}
-		int *p=*(arr+i);
-		shift_element(*(arr+j),counter);
-		*(arr+j)=*p;
-	}
-	
+
+j=i-counter;
+
+  int x;
+		x=*(arr+i);
+		shift_element((arr+j),counter);
+		*(arr+j)=x;
+
+
+}	
 		
 }
