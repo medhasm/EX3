a5fedf323883ef0715990968c57612450ffe3654
medhasm <57920502+medhasm@users.noreply.github.com>
2019-12-18 14:11:39 +0200

    Update Insert.h

--- Insert.h
+++ Insert.h
@@ -1,7 +1,8 @@
 #ifndef _INSERT_H_
 #define _INSERT_H_
 #define Lenght 50
-double Array[Lenght]={0};
+
+int Array[Lenght];
 void shift_element(int* arr, int i);
 void insertion_sort(int* arr , int len);
 #endif
