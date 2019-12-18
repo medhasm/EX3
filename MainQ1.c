c2202723212fcf072e1b8612b2a3386a3ce7010f
medhasm <57920502+medhasm@users.noreply.github.com>
2019-12-18 14:12:28 +0200

    Update MainQ1.c

--- MainQ1.c
+++ MainQ1.c
@@ -2,15 +2,15 @@
 #include "Insert.h"
 #define Lenght 50
 int main(){
-double Array[Lenght]={0};
+int Array[Lenght]={0};
 for(int i=0;i<Lenght;i++){
 printf("Enter An Integer To Cell %d : \n",i);
 scanf("%d",&*(Array + i));
 }
-int *p;
-p=Array;
-insertion_sort(*p ,Lenght);
+
+insertion_sort(Array ,Lenght);
 for(int i=0;i<Lenght;i++){
-	printf("Array[%d]= %d,",i,*(p+i));
+	printf("Array[%d]= %d \n",i,*(Array+i));
 }
+return 0;
 }
